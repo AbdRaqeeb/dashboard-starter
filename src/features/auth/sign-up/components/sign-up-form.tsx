9e1404ff7aafe2f3b12f7fae6b631ed3d06efc49
abdur-raqeeb ajao <ajaorqb@gmail.com>
2025-10-24 21:45:33 -0600

    fix(ci): lint errors

--- src/features/auth/sign-up/components/sign-up-form.tsx
+++ src/features/auth/sign-up/components/sign-up-form.tsx
@@ -34,10 +34,8 @@ export function SignUpForm({ className, ...props }: React.HTMLAttributes<HTMLFor
 		},
 	});
 
-	function onSubmit(data: z.infer<typeof formSchema>) {
+	function onSubmit(_data: z.infer<typeof formSchema>) {
 		setIsLoading(true);
-		// eslint-disable-next-line no-console
-		console.log(data);
 
 		setTimeout(() => {
 			setIsLoading(false);
--- src/lib/handle-server-error.ts
+++ src/lib/handle-server-error.ts
@@ -2,9 +2,6 @@ import { AxiosError } from 'axios';
 import { toast } from 'sonner';
 
 export function handleServerError(error: unknown) {
-	// eslint-disable-next-line no-console
-	console.log(error);
-
 	let errMsg = 'Something went wrong!';
 
 	if (error && typeof error === 'object' && 'status' in error && Number(error.status) === 204) {
